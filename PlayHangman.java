f5aa25f6b390887a8bc76c93d2cde36e64d22aac
Alexandros Patsalides <apatsa01@ucy.ac.cy>
2021-04-14 19:09:32 +0300

    New class PlayHangman

---
+++ PlayHangman.java
@@ -0,0 +1,65 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class PlayHangman {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int length = 0;
+        int guessNum = 0;
+        ArrayList<String> dictionary = new ArrayList<String>();
+        char guess;
+
+        try(Scanner fileReader = new Scanner(new File(args[0]))){
+            while(fileReader.hasNextLine()){
+                dictionary.add(fileReader.nextLine());
+            }    
+        }
+        catch(FileNotFoundException e){
+                e.printStackTrace();
+        }
+
+        length = Integer.parseInt(args[1]);
+        guessNum = Integer.parseInt(args[2]);
+        
+        System.out.println("Welcome to Hangman game");
+        System.out.println("You choose to use length word of: "+ length);
+        System.out.println("You choose to use maximum of tries = "+guessNum);
+
+        Hangman game = new Hangman(length, guessNum, dictionary);       
+        System.out.println("guesses: "+ game.getTriesAmount());
+        System.out.println("words: " + game.dictionarySize());
+        System.out.println("guessed:" + game.getGuesses());
+    	game.printCurrentState();  
+    	
+      
+        while(!game.exitGame()){
+                  
+        	System.out.println("Your guess? ");
+            guess = checkLetterInput(sc);
+            if(game.checkDoublicate(guess)) {
+            	System.out.println("You already guessed that!");
+            	System.out.println();
+            }
+            else {
+            if(game.playGame(guess)){
+                System.out.println("Your guess was right!");
+            }
+            else{
+                System.out.println("Sorry, there are no "+ guess+"'s");
+            }
+
+            System.out.println();
+            }
+           
+            
+            System.out.println("guesses: "+ game.getTriesAmount());
+            System.out.println("words: " + game.dictionarySize());
+            System.out.print("guessed: ");
+            System.out.println(game.getGuesses());
+            game.printCurrentState();
+            
+        }
