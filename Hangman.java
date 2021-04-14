bc9ea77860d17b73e794a99d4b2b06ae71b0ce3e
Alexandros Patsalides <apatsa01@ucy.ac.cy>
2021-04-15 01:46:39 +0300

    Added comments to some methods in the Hangman class.

--- Hangman.java
+++ Hangman.java
@@ -2,6 +2,17 @@
 import java.util.*;
 import java.util.stream.Collectors;
 
+/**
+ * @author Alexandros Patsalides / 1028348
+ * @author Christodoulos Mavrides / 
+ * 
+ * @version 3.0
+ * 
+ * This method represents the object Hangman - a very well known game;
+ * It is not a fair game because the computer hides the guessing process in order to play not by
+ * the rules but in a way that makes it super hard for the user to win.
+ *
+ */
 public class Hangman {
 	  private ArrayList<String> dictionary;
 	  private ArrayList<Character> guesses = new ArrayList<>();
@@ -10,7 +21,16 @@ public class Hangman {
 	  private String output;
 	  private String answer;
 	  
-	  public Hangman(int amountOfLetters, int amountOfTries, ArrayList<String> Dictionary) {
+	  /**
+	   * The constructor of the class Hangman that initializes the ArrayList Dictionary,
+	   * the ArrayList guesses, the letterAmount and the triesAmount;
+	   * Initializes the dictionary using words of given length in the desired dictionary;
+	   * 
+	 * @param amountOfLetters The amount of letters in the desired word; This will be used to filter the words in the given dictionary.
+	 * @param amountOfTries The amount of allowed guesses for the user.
+	 * @param Dictionary An arrayList representing all the valid words.
+	 */
+	public Hangman(int amountOfLetters, int amountOfTries, ArrayList<String> Dictionary) {
 		  dictionary = new ArrayList<>();
 		  this.letterAmount = amountOfLetters;
 		  this.triesAmount = amountOfTries;
@@ -21,29 +41,48 @@ public class Hangman {
 		 this.output = s;
 		  dictionary.addAll(Dictionary.stream().filter(word -> word.length() == this.letterAmount).collect(Collectors.toList()));	  
 	  }
-	  public boolean exitGame() {
+	  /**
+	   * Checks if the game has ended; This is true if
+	   * there are no more guesses left or if the dictionary empties or if the player won in the previous round.
+	 * @return
+	 */
+	public boolean exitGame() {
 		  return this.triesAmount == 0  || !output.contains("_") || dictionary.isEmpty();
 	  }
 	  
-	  public int getTriesAmount() {
+	  /**
+	 * @return Return the amount of tries (guesses) left.
+	 */
+	public int getTriesAmount() {
 		  return this.triesAmount;
 	  }
 	  
-	  public int getAmountOfRemainingWords() {
+	  /**
+	 * @return Returns the amount of available words in the dictionary
+	 */
+	public int getAmountOfRemainingWords() { 
 		  return this.dictionary.size();
 	  }
 	  
-	  public boolean Winner() {
+	  /**
+	   * Check if the player won the game.
+	 * @return Returns true if the player won; Otherwise it returns false.
+	 */
+	public boolean Winner() {
+		  
 	        if (!exitGame()) {
-	            System.out.println("Game not over yet");
+	            System.out.println("The game is not over yet");
 	            return false;
-	        } 
-	        else {
-	            return !output.contains("_");
 	        }
+	       
+	      return !output.contains("_");
+	        
 	    }
 	  
-	  public void printCurrentState() {
+	  /**
+	 * This method prints the current state of the chosen word; In other words it prints the spaces remaining to find the current word.
+	 */
+	public void printCurrentState() {
 		  String s = "";
 		  for(int i=0;i<this.output.length()-1;i++)
 			  s+=output.charAt(i)+",";
@@ -51,27 +90,42 @@ public class Hangman {
 		  System.out.println(s);
 	  }
 	  
-	  public int dictionarySize() {
+	  /**
+	 * @return Return the amount of words left in the Dictionary.
+	 */
+	public int dictionarySize() {
 		  return dictionary.size();
 	  }
 	  
 	  
-	  public String getGuesses() {
+	  /**
+	 * @return Returns the guesses of the user using the toString method of the ArrayList
+	 */
+	public String getGuesses() {
 		  return this.guesses.toString();
 	  }
 	  
-	  public boolean checkDoublicate(char guess) {
+	  /**
+	   * This method checks the guesses of the user for a duplicated input.
+	 * @param guess The current guess of the user
+	 * @return Returns true if the guessed character has already been guessed; Otherwise it returns false.
+	 */
+	public boolean checkDoublicate(char guess) {
 		  for(Character a : this.guesses)
 	        	if(a == guess)
 	        		return true;
 		  return false;
 	  }
 	  
-	  public String getAnswer() {
+	  /**
+	   * This method is invoked only when the game is over; Returns the answer if the game has ended;
+	   * Otherwise it returns null.
+	 * @return The word "selected" by the program; If the game is not over return null.
+	 */
+	public String getAnswer() {
 		  if(exitGame())
 			  return answer;
-			  else
-				  return null;
+		  return null;
 		  
 	  }	 
 	  
@@ -140,6 +194,14 @@ public class Hangman {
 	  }
 	  
 	  
+	  public void printSums() {
+		  System.out.println("guesses: "+ getTriesAmount());
+	        System.out.println("words: " + dictionarySize());
+	        System.out.println("guessed:" + getGuesses());
+	    	printCurrentState(); 
+	    	System.out.println();
+	  }
+	  
 	  private int getUniqueChars(ArrayList<String> words) {
 	        int counter = 0;
 	        if (words == null) return 0;
@@ -155,12 +217,4 @@ public class Hangman {
 	        }
 	        return counter;
 	    }
-
-	public void printSums() {
-		  System.out.println("guesses: "+ getTriesAmount());
-	        System.out.println("words: " + dictionarySize());
-	        System.out.println("guessed:" + getGuesses());
-	    	printCurrentState(); 
-	    	System.out.println();
-	  }
 }
